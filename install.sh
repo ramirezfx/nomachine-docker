7d83d3e60df2e64b719feb8f67e50af656141df1
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-11-30 09:45:58 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir /usr/share/icons/docker
-sudo cp resources/startnomachine.sh /usr/local/bin
-sudo cp resources/nomachine-docker.desktop /usr/share/applications
-sudo cp resources/nomachine.png /usr/share/icons/docker
