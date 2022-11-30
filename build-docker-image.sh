ea1d474ddfb124befb9b085dfd5afb2693b924ea
ramirezfx <68792418+ramirezfx@users.noreply.github.com>
2022-11-30 09:45:40 +0100

    Delete build-docker-image.sh

--- build-docker-image.sh
+++
@@ -1,6 +0,0 @@
-echo "TAG (example: nomachine:latest)"
-read tag
-rm nomachine_6.11.2_1_amd64.deb
-wget https://download.nomachine.com/download/6.11/Linux/nomachine_6.11.2_1_amd64.deb
-sudo docker build -t $tag .
-echo "Build finished"
